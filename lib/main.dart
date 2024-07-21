1e5568db9141e4873b7a6a494bb5734865721c42
Vikas-garg-tiet26 <vgarg1_be22@thapar.edu>
2024-07-21 18:27:21 +0530

    meals page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,8 +24,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
-       
+      home: const MealsScreen(
+        title: 'Some Category...',
+        meals: [],
+      ),
     );
   }
 }
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummydata.dart';
+import 'package:meals/Models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
 
+  final String title;
+  final List<Meal> meals;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget content = ListView.builder(
+      itemBuilder: (ctx, index) => Text(meals[index].title),
+    );
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh ... noting here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Try Selecting a different category',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+          ],
+        ),
+      );
+    }
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+          itemBuilder: (ctx, index) => Text(
+                meals[index].title,
+              ));
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) => Text(
+          meals[index].title,
+        ),
+      ),
+    );
   }
 }
